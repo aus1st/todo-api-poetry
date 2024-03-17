f764b047aa442856a0dede993e1321b1c822b1be
Ahmed Siddiqui <aus1st@gmail.com>
2024-03-17 16:58:06 +0500

    db related

---
+++ src/database/db.py
@@ -0,0 +1,5 @@
+
+import os
+#from dotenv import load_dotenv
+
+DB_URI="postgresql://aus1st:Me1K5NJWijTr@ep-dark-scene-148946-pooler.us-east-2.aws.neon.tech/todo_fast?sslmode=require"
---
+++ src/settings.py
@@ -0,0 +1,1 @@
+from startlett.config import config
